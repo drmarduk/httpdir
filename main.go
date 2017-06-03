7cd217267cd35d497ff1771e49ccd1874974967c
drmarduk <drmarduk@gmail.com>
2017-06-03 15:32:07 +0200

    added some comments

--- main.go
+++ main.go
@@ -8,19 +8,22 @@ import (
 )
 
 var (
-	cookievalue string
-	noauth      bool
+	cookievalue string // holds the hash of the auth password in browser cookie
+	noauth      bool   // wether to use a cookie based auth login
 )
 
 func main() {
+	// main http conf
 	host := flag.String("host", "", "hostname to listen on")
 	port := flag.Int("port", 8080, "port number to listen on")
 	root := flag.String("root", ".", "folder which we serve")
+	// auth stuff
 	pass := flag.String("pass", generateKey(), "the key to enter the directory")
+	auth := flag.Bool("noauth", false, "if true == no auth")
+	// TLS stuff
 	cert := flag.String("cert", "server.crt", "the filename of the server certificate")
 	key := flag.String("key", "server.key", "the filename of the server key")
-	tls := flag.Bool("tls", true ,"wether to use tls")
-	auth := flag.Bool("noauth", false, "if true == no auth")
+	tls := flag.Bool("tls", true, "wether to use tls")
 	flag.Parse()
 
 	// TODO: meeh, we do not want global vars
